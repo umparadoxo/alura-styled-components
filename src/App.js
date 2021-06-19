a38aa6e8339a30ac34d79dbb3ede5ce281c32333
umparadoxo <legiaored@gmail.com>
2021-06-18 23:36:51 -0300

    Icones
    adicionados icones e categorias

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import { GlobalStyle } from './Components/GlobalStyle'
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Cabecalho />
       <Container />
     </>
---
+++ src/Components/Extrato.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Box, Botao } from '../Components/UI'
+import { extratoLista } from '../info'
+import Items from './Items'
+
+
+const Extrato = () => {
+
+    return (
+        <Box>
+            {
+                extratoLista.updates.map(({ id, type, value, from, date }) => {
+                    return (
+                        <Items key={id} type={type} value={value} from={from} date={date} />
+                    )
+                })
+            }
+            <Botao>Ver mais</Botao>
+        </Box>
+    )
+}
+
+export default Extrato;
